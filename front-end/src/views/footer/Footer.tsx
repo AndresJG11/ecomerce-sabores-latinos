3b1adcb1ab93a51ae31785e89eb6fb7152d7ea92
Andrés JG <anjimenez11@hotmail.com>
2020-12-30 00:01:03 -0500

    cambios
    No se :'v

--- front-end/src/views/footer/Footer.tsx
+++ front-end/src/views/footer/Footer.tsx
@@ -1,16 +1,26 @@
 import { Routes } from 'constantes'
 import { Link } from 'react-router-dom'
+import FacebokLogo from 'assets/icons/facebook.svg'
+import WhatsappLogo from 'assets/icons/whatsapp.svg'
 import './footer-styles.css'
 
 export const Footer = () => {
     return (
-        <div className="bg-white">
+        <footer>
            <ul className="footer-items"> 
                 <li> Términos y condiciones </li> 
-                <Link className="no-link" to={Routes.contacto}> <li> Contacto </li> </Link>
+                
+                <div>
+                    <Link className="no-link" to={Routes.contacto}> <li> Contacto </li> </Link>
+                    <div>
+                        <img className="contact-icon" src={FacebokLogo} alt=""/>
+                        <img className="contact-icon" src={WhatsappLogo} alt=""/>
+                    </div>
+                </div>
+                
                 <Link className="no-link" to={Routes.nosotros}> <li> Nosotros </li> </Link>
                 <Link className="no-link" to={Routes.carrito}> <li> Mi carrito de compras </li> </Link>
            </ul>
-        </div>
+        </footer>
     )
 }
